fa2ba4e2c401bd38553a7931ccd08b692f06341a
Johan Hardy <johan.hardy@spacebel.be>
2015-10-07 17:15:43 +0200

    [479249] Cannot generate outside the workspace
    The absolute path of the filesystem is passed to the Abstract Acceleo
    generator at launch delegate and selection of elements.
    
    Bug: 479249
    Change-Id: I527553e39d4acfdaf52b0e76900f276e32a9a8f6
    Signed-off-by: Johan Hardy <johan.hardy@spacebel.be>

--- org.eclipse.umlgen.gen.embedded.c/plugins/org.eclipse.umlgen.gen.embedded.c.ui/src/org/eclipse/umlgen/gen/embedded/c/ui/handler/GenerateEmbeddedCHandler.java
+++ org.eclipse.umlgen.gen.embedded.c/plugins/org.eclipse.umlgen.gen.embedded.c.ui/src/org/eclipse/umlgen/gen/embedded/c/ui/handler/GenerateEmbeddedCHandler.java
@@ -21,6 +21,7 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
@@ -131,8 +132,9 @@ public class GenerateEmbeddedCHandler extends AbstractHandler {
 
         try {
             /* Build the output path */
-            outputFolderPath = new Path(ResourcesPlugin.getWorkspace().getRoot().getLocation().toString()
-                    + configuration.getAttribute(IUML2ECConstants.OUTPUT_FOLDER_PATH, ""));
+            IFolder folder = ResourcesPlugin.getWorkspace().getRoot().getFolder(new Path(configuration
+                    .getAttribute(IUML2ECConstants.OUTPUT_FOLDER_PATH, "")));
+            outputFolderPath = new Path(folder.getRawLocation().toString());
         } catch (CoreException e) {
             IStatus status = new Status(IStatus.ERROR, UML2ECUIActivator.PLUGIN_ID, e.getMessage(), e);
             UML2ECUIActivator.getDefault().getLog().log(status);
